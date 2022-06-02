d3c71f3ea72cdbe243960b6e73f337f389d42a62
Scott Johnson <singinbaritone@gmail.com>
2022-06-01 23:51:16 -0400

    finished seeding ingredients and added store and api to appropriate index files

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,8 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
-import auth from './auth'
+import auth from './auth';
+import ingredients from './ingredients';
 
 const reducer = combineReducers({ auth })
 const middleware = composeWithDevTools(
@@ -11,4 +12,5 @@ const middleware = composeWithDevTools(
 const store = createStore(reducer, middleware)
 
 export default store
-export * from './auth'
+export * from './auth';
+export * from './ingredients';
