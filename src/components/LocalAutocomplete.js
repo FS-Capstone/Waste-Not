fdfa171ee21bcfab15b673d9ec2594fcff6fd53d
Scott Johnson <singinbaritone@gmail.com>
2022-06-02 21:09:26 -0400

    initial autocomplete framework

---
+++ src/components/LocalAutocomplete.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {TextField, Autocomplete} from '@mui/icons-material';
+import addPantryItem from '../store';
+
+const PantryAutocomplete = ({searchOptions, searchName, selectedPantry}) => {
+  const [ingredient, setIngredient] = useState(searchOptions[0])
+  const dispatch = useDispatch();
+  const user = useSelector(state=>state.auth)
+  const localPantry = window.localStorage.getItem('pantry')
+  const handleChange = e => {
+    e.preventDefault();
+    const newPantryItem = e.target.value
+    if(!user.id){
+      localPantry.push(newPantryItem)
+    }
+    dispatch(addPantryItem(newPantryItem.id, selectedPantry.id))
+  }
+  
+  return(
+    <Autocomplete
+      disablePortal
+      autoHighlight
+      value={ingredient}
+      onChange={(e)=> handleChange(e)}
+      id={searchName}
+      options={searchOptions}
+      getOptionLabel={(option) => option.ingredient}
+      renderInput={(params)=> <TextField {...params} label='Search...'/> }
+    />
+  )
+}
+
+export default PantryAutocomplete
