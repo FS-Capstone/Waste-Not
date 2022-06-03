f37fa8298c1739cc4d9978ed36da13ed009f6df2
Zach Rizzo <RizzoZachary2@gmail.com>
2022-06-03 15:21:28 -0500

    working on selecting different pantries

--- src/components/account/AccountPage.js
+++ src/components/account/AccountPage.js
@@ -1,9 +1,24 @@
+import { Box, Select } from "@mui/material";
 import React from "react";
+import { useState } from "react";
 
 
 const AccountPage = () => {
+  const [selectedPantry, setSelectedPantry] = useState({})
+
   return(
-    <h1>Account</h1>
+    <Box>
+      <h1>Account Page</h1>
+      <Select
+        labelId="selected-pantry"
+        id="selected-pantry"
+        value={selectedPantry}
+        label="Age"
+        onChange={(evt) => setSelectedPantry(evt.target.value)}
+        >
+          
+      </Select>
+    </Box>
   )
 }
 
