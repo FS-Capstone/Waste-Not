542fffa6119e0e25ab2768f3e1bedf49a895ebbb
Zach Rizzo <RizzoZachary2@gmail.com>
2022-06-04 17:36:03 -0500

    created select component for selecting what pantry is active

--- server/api/pantry.js
+++ server/api/pantry.js
@@ -13,3 +13,16 @@ router.get('/', async (req, res, next) => {
     next(error);
   }
 })
+
+
+router.put('/changeSelectedPantry', async (req, res, next) => {
+  try{
+    const user = await User.findByToken(req.headers.authorization);
+    user.currentlySelectedPantryId = req.body.newSelectedPantryId;
+    await user.save();
+    res.send(user);
+  }
+  catch(error){
+    next(error);
+  }
+})
--- src/components/account/AccountPage.js
+++ src/components/account/AccountPage.js
@@ -1,24 +1,8 @@
-import { Box, Select } from "@mui/material";
 import React from "react";
-import { useState } from "react";
-
 
 const AccountPage = () => {
-  const [selectedPantry, setSelectedPantry] = useState({})
-
   return(
-    <Box>
-      <h1>Account Page</h1>
-      <Select
-        labelId="selected-pantry"
-        id="selected-pantry"
-        value={selectedPantry}
-        label="Age"
-        onChange={(evt) => setSelectedPantry(evt.target.value)}
-        >
-          
-      </Select>
-    </Box>
+    <h1>ACCOUNT PAGE</h1>
   )
 }
 
