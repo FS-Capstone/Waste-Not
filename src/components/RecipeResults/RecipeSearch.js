501b60d3cbd1cf9e13558cb59318bebf7b1785a3
Scott Johnson <singinbaritone@gmail.com>
2022-06-07 12:03:21 -0400

    initial commit for recipeSearch

---
+++ src/components/RecipeResults/RecipeSearch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+import SearchResults from './SearchResults';
+
+const RecipeSearch = () => {
+
+  return(
+    <hr/>
+  )
+};
+
+export default RecipeSearch;
