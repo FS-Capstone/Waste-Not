9a4b4fce14b82af88be08cbd7e2aaf87802da9cf
Janae Edwards <edwards.janae@gmail.com>
2022-06-07 21:50:56 -0400

    tbd

--- src/components/Wine/BrandResults.js
+++ src/components/Wine/BrandResults.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+//const API_KEY = process.env.REACT_APP_API_KEY
+
 const BrandResults = (props) => {
 
 const { value } = props
@@ -11,26 +13,34 @@ const options = {
     params: {wine: `${value}`, maxPrice: '50', minRating: '0.7', number: '3'},
     headers: {
       'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+      'X-RapidAPI-Key': 'API KEY HERE'
     }
   };
 
-  axios.request(options).then(function (response) {
-      console.log(response.data);
-      console.log(response.data.recommendedWines.length)
-    ;  
+//  const results = axios.request(options).catch(function (error) {
+//       console.error(error);
+//   });
 
-  }).catch(function (error) {
-      console.error(error);
-  });
+ axios.request(options).then(function (response) {
+	console.log(response.data);
+    console.log(response.data.recommendedWines)
+    
+}).catch(function (error) {
+	console.error(error);
+});
 
-
+//const results = response.data.recommendedWines
   
 return (
     <div>
         <h3> You Selected: </h3>
         { value }
         <div>
-            
+            {/* { results.map(result => { 
+                return (
+                <li> {result.title} </li>
+                )
+            }) } */}
         </div>
     </div>
 )
