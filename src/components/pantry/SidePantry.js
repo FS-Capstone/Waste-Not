7e30f1bd4891d786468783077c3ea4795b440913
Zach Rizzo <RizzoZachary2@gmail.com>
2022-06-14 18:39:37 -0500

    working on categories for sidepantry

--- src/components/pantry/SidePantry.js
+++ src/components/pantry/SidePantry.js
@@ -1,33 +1,85 @@
-import React, {useEffect} from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Box, List, Button  } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Box, List, ListItem, ListItemButton } from "@mui/material";
 import { PantryItem } from "./PantryItem";
 import Divider from '@mui/material/Divider';
 import PantryAutocomplete from "../PantryAutocomplete";
 import { ingredientList } from "../../../script/seedData";
+import Collapse from '@mui/material/Collapse';
 
-import axios from 'axios';
+
+const getAllCategories = (ingredients) => {
+  const mapCategoriesToIngredients = {};
+
+  ingredients.forEach(ingredient => {
+    const broadCategory = ingredient.broadCategory;
+    if(mapCategoriesToIngredients[broadCategory]){
+      mapCategoriesToIngredients[broadCategory].push(ingredient);
+    }
+    else{
+      mapCategoriesToIngredients[broadCategory] = [ingredient];
+    }
+  })
+  return mapCategoriesToIngredients;
+}
 
 export default function SidePantry() {
   const pantry = useSelector(state => state.selectedPantry);
   const ingredientsInPantry = pantry?.ingredients;
+  const [openedCategories, setOpenedCategories] = useState({});
+
+
+  useEffect(() => {
+    if(!ingredientsInPantry)
+      return null;
+    const categoriesWithIngredients = getAllCategories(ingredientsInPantry);
+    const categories = Object.keys(categoriesWithIngredients);
+    for(let category of categories){
+      setOpenedCategories({...openedCategories, [category]:false});
+    }
+  }, [])
+
 
   if(!ingredientsInPantry)
     return null;
 
+
+
+
+
+  console.log(openedCategories);
   return(
     <Box>
       <PantryAutocomplete searchOptions={ingredientList} searchName='pantrySearch' selectedPantry={pantry} />
 
       <Divider/>
 
-      <List>
+      {Object.keys(categoriesWithIngredients).map(category => {
+        return (
+          <ListItemButton key={category}>
+            {category}
+          </ListItemButton>
+          // <Collapse>
+            // <List>
+            //   <div>{category}</div>
+            // </List>
+          // </Collapse>
+
+          // <Collapse key={category}>
+          //   <List>
+
+          //   </List>
+          // </Collapse>
+        )
+      })}
+
+      {/* <List>
         {
           ingredientsInPantry.map(ingredient => {
             return <PantryItem ingredient={ingredient} key={ingredient.id}/>
           })
         }
-      </List>
+      </List> */}
     </Box>
 
 
