73beb5d77bf51381c6db801a8aa7d9b9fb42de58
Janae Edwards <edwards.janae@gmail.com>
2022-06-16 19:23:00 -0400

    added Recipe model and associations

---
+++ server/db/models/Recipe.js
@@ -0,0 +1,44 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Recipe = db.define('recipe', {
+    id: {
+        type: Sequelize.INTEGER, // does scott need recipe id property for faves?
+    },
+    title: {
+        type: Sequelize.STRING,
+    },
+    author: {
+        type: Sequelize.STRING,
+    },
+    cuisine: {
+        type: Sequelize.STRING,
+    },
+    prepTime: {
+        type: Sequelize.STRING,
+    },
+    cookTime: {
+        type: Sequelize.STRING,
+    },
+    servings: {
+        type: Sequelize.INTEGER,
+    },
+    ingredients: {
+        type: Sequelize.STRING,
+    },
+    instructions: {
+        type: Sequelize.STRING,
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+    },
+    recipeUrl: {
+        type: Sequelize.STRING
+    },
+    createdByUser: { // only required property, to sort btw faves and created
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+    }
+});
+
+module.exports = Recipe 
