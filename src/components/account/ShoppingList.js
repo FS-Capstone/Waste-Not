8913b1b6e670d03bed11f3b3d913592c69abbae9
Scott Johnson <singinbaritone@gmail.com>
2022-06-17 17:00:10 -0400

    added print and download functionality

--- src/components/account/ShoppingList.js
+++ src/components/account/ShoppingList.js
@@ -1,5 +1,8 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
+import html2canvas from 'html2canvas';
+import {jsPDF} from 'jspdf';
+import print from 'print-js';
 import {Paper, Box, Button, Typography, IconButton} from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import {ingredientList} from '../../../script/seedData';
@@ -16,6 +19,7 @@ const ShoppingList = () => {
   const pantry = useSelector(state => state.selectedPantry)
   const [hidden, setHidden] = useState(false)
   const [selected, setSelected] = useState([]);
+  const printRef = useRef();
 
 
   const handleDelete = (e, ingredient) => {
@@ -39,23 +43,55 @@ const ShoppingList = () => {
     dispatch(deleteMultipleShoppingItems(selected, user.id))
   }
 
+  const hider = () => {
+    setHidden(true)
+  }
+
+  const handleExport = async(e) => {
+    e.preventDefault();
+    await hider()
+    const element = printRef.current;
+    const canvas = await html2canvas(element);
+    const data = canvas.toDataURL('image/png');
+
+    const pdf = new jsPDF();
+    const imgProperties = pdf.getImageProperties(data);
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+    const pdfHeight = (imgProperties.height * pdfWidth) / imgProperties.width;
+
+    pdf.addImage(data, 'PNG', 0, 0, pdfWidth, pdfHeight);
+    if(e.target.name === 'download') {
+      pdf.save('Shopping List.pdf')
+    }
+    else {
+      const printData = pdf.output('blob');
+      const blobUrl = URL.createObjectURL(printData);
+      print(blobUrl)
+    }
+    setHidden(false)
+  }
+
   return (
     <Box sx={{display:'flex', marginTop:'5vh', flexDirection:'column', height:'auto', alignItems:'center', width:'100vw'}}>
-      <Paper sx={{display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center', marginBottom:'2vh', height:'100%', width:'100%', maxWidth:'800px', minWidth:'360px', backgroundSize:'cover', backgroundRepeat:'no-repeat', backgroundPosition:'center'}}>
+      <Paper ref={printRef} sx={{display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center', marginBottom:'2vh', height:'100%', width:'100%', maxWidth:'800px', minWidth:'360px', backgroundSize:'cover', backgroundRepeat:'no-repeat', backgroundPosition:'center'}}>
         <Typography variant='h2' sx={{marginTop:'1rem', marginBottom:'1rem', fontFamily:'Kalam', borderBottom:'3px solid black'}}>Shopping List</Typography>
         <Box sx={{display:'flex', flexDirection:'column', width:'80%', marginBottom:'1rem'}}>
           {
             ingredients.map((ingredient) => (
-              <Box sx={{display:'flex', borderBottom:'3px solid black', marginBottom:'.5rem'}}> 
-                <Typography key={ingredient.id} sx={{fontFamily:'Kalam', cursor:'pointer', textDecoration: selected.includes(ingredient) ? 'line-through' : '' }} variant='h4' onClick={(e) => handleSelect(e, ingredient)}>{ingredient.name}</Typography> 
+              <Box key={ingredient.id} sx={{display:'flex', borderBottom:'3px solid black', marginBottom:'.5rem'}}> 
+                <Typography sx={{fontFamily:'Kalam', cursor:'pointer', textDecoration: selected.includes(ingredient) ? 'line-through' : '' }} variant='h4' onClick={(e) => handleSelect(e, ingredient)}>{ingredient.name}</Typography> 
                 {!hidden ? <IconButton onClick={(e)=> handleDelete(e, ingredient)} sx={{marginLeft:'auto'}}><DeleteIcon/></IconButton> : null} 
               </Box>))
           }
         </Box>
       </Paper>
       {!selected.length ? null : <Button sx={{margin:'1rem'}} variant='outlined' onClick={(e)=>handleClick(e)}>Add Selected to Pantry</Button>}
-      <Box>
-      <PantryAutocomplete searchOptions={ingredientList} searchName='shoppingListSearch' selectedPantry={pantry} />
+      <Box sx={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+        <Box sx={{display:'flex', marginBottom:'1rem', width:'100%', justifyContent:'space-around'}}>
+          <Button onClick={handleExport} name='download' variant='contained'>Download as PDF</Button>
+          <Button onClick={handleExport} name='print' variant='contained'>Print</Button>
+        </Box>
+        <PantryAutocomplete searchOptions={ingredientList} searchName='shoppingListSearch' selectedPantry={pantry} />
       </Box>
     </Box>
 
