ed8675c99e27ef3aa9176da26358c1b7653ddefd
Janae Edwards <edwards.janae@gmail.com>
2022-06-22 13:40:20 -0400

    new font and page content styled

--- src/components/LandingPageContent.js
+++ src/components/LandingPageContent.js
@@ -4,24 +4,32 @@ import { CssBaseline } from "@mui/material";
 import { styled } from '@mui/system';
 
 const LandingContent = styled('div')({
-    color: 'white',
-    display: 'flex',
-    height: '90vh',
-    alignItems: 'center',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    fontFamily: 'Nunito',
+        color: 'white',
+        display: 'flex',
+        height: '90vh',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        fontFamily: 'Nunito',
+        h1: {
+           fontSize: '3rem',
+           margin: '0' 
+        },
+        h2: {
+            fontSize: '2rem'
+        }
 })
 
 const LandingPageContent = () => {
     return (
         <LandingContent>
-            <div><h1> Welcome to our Capstone Project</h1></div>
-             <div><h2> Mission Statement </h2></div>
-             <div><h3> Statistics etc. </h3></div>
-             <Link to='/home'> <button> Enter Site </button> </Link>
+            <Collapse>
+            <h1> Welcome to our Capstone Project</h1>
+            <h2> (Tagline) </h2>
+            <div><Link to='/home'> <button> Enter Site </button> </Link></div>
+            </Collapse>
         </LandingContent>
     )
 }
 
-export default LandingPageContent
+export default LandingPageContent;
