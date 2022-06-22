7d9dee22e22da9c84f19107c4588bd7e40765fb4
Janae Edwards <edwards.janae@gmail.com>
2022-06-22 14:47:06 -0400

    remove comments

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 import { styled } from '@mui/system';
-import { Box } from "@mui/system";
 import LandingPageContent from './LandingPageContent';
 
 const LandingBackground = styled('div')({
@@ -26,10 +24,4 @@ const LandingPage = () => {
 
 export default LandingPage; 
 
-        // <div className='landing'>
-        //     <CssBaseline />
-        //     <div><h1> Welcome to our Capstone Project</h1></div>
-        //     <div><h2> Mission Statement </h2></div>
-        //     <div><h3> Statistics etc. </h3></div>
-        //     <Link to='/home'> <button> Enter Site </button> </Link>
-        // </div> 
+    
