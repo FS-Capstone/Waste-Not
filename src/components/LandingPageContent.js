38baf313b0067b3d81c96b13df0d24359810345a
Jennifer Woodbury <jenann125@yahoo.com>
2022-06-23 20:14:51 -0400

    Code change to replace home with landing page

--- src/components/LandingPageContent.js
+++ src/components/LandingPageContent.js
@@ -1,52 +1,52 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { styled } from '@mui/system';
+import { styled } from "@mui/system";
 import { Collapse } from "@mui/material";
-import { Button } from "@mui/material";
 
-
-const LandingContent = styled('div')({
-        color: 'white',
-        display: 'flex',
-        height: '90vh',
-        alignItems: 'center',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        fontFamily: 'Nunito',
-        h1: {
-           fontSize: '4rem',
-           margin: '0' 
-        },
-        h2: {
-            fontSize: '2rem',
-            textAlign: 'center'
-        },
-        div: {
-            justifyContent: 'center',
-            textAlign: 'center',
-            textDecoration: 'none'
-        },
-        
-})
+const LandingContent = styled("div")({
+  color: "white",
+  display: "flex",
+  height: "90vh",
+  alignItems: "center",
+  flexDirection: "column",
+  justifyContent: "center",
+  fontFamily: "Nunito",
+  h1: {
+    fontSize: "4rem",
+    margin: "0",
+  },
+  h2: {
+    fontSize: "2rem",
+    textAlign: "center",
+  },
+  div: {
+    justifyContent: "center",
+    textAlign: "center",
+    textDecoration: "none",
+  },
+});
 
 const LandingPageContent = () => {
-    const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useState(false);
 
-    useEffect(() => {
-        setChecked(true);
-    },[]); 
+  useEffect(() => {
+    setChecked(true);
+  }, []);
 
-    return (
-        <LandingContent>
-         <Collapse in={checked} {...(checked ? { timeout: 1500 } : {})} collapsedheight={50}>
-            <div>
-            <h1> Welcome to Our Capstone Project </h1>
-            <h2> (Tagline) </h2>
-            <Link to='/home'> <Button variant='contained'> Enter </Button></Link>
-            </div>
-          </Collapse> 
-        </LandingContent>
-    )
-}
+  return (
+    <LandingContent>
+      <Collapse
+        in={checked}
+        {...(checked ? { timeout: 1500 } : {})}
+        collapsedheight={50}
+      >
+        <div>
+          <h1> Welcome to Our Capstone Project </h1>
+          <h2> (Tagline) </h2>
+        </div>
+      </Collapse>
+    </LandingContent>
+  );
+};
 
 export default LandingPageContent;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,44 +1,39 @@
 import React, { useState } from "react";
 import { Link as RouterLink, useNavigate, useLocation } from "react-router-dom";
-import HomeIcon from '@mui/icons-material/Home';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import { styled } from '@mui/material/styles';
-import IconButton from '@mui/material/IconButton';
+import HomeIcon from "@mui/icons-material/Home";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
 import { Link as MaterialLink } from "@mui/material";
 import { useTheme } from "@emotion/react";
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
 import AccountMenu from "./AccountMenu";
 import PantryDrawer from "../pantry/PantryDrawer";
 
 //Container for links in nav bar
-const LinkContainer = styled(Box)(({theme}) => {
-  return{
+const LinkContainer = styled(Box)(({ theme }) => {
+  return {
     width: "100%",
-    display: 'flex',
-    justifyContent: 'center'
-  }
+    display: "flex",
+    justifyContent: "center",
+  };
 });
 
 //Used for links in nav bar
-const Link = styled(MaterialLink)(({theme}) => {
-  return{
+const Link = styled(MaterialLink)(({ theme }) => {
+  return {
     fontSize: "1.3em",
     color: theme.palette.text.disabled,
-    margin: '0 14px 0 14px',
+    margin: "0 14px 0 14px",
     "&:hover": {
       color: theme.palette.text.primary,
-      textDecoration: "none"
-    }
-  }
+      textDecoration: "none",
+    },
+  };
 });
-
-
-
-
-
 
 const Navbar = (props) => {
   const navigate = useNavigate();
@@ -47,87 +42,104 @@ const Navbar = (props) => {
 
   //anchor point for account menu
   const [anchorEl, setAnchorEl] = useState(null);
-  const [pantryDrawerOpen, setPantryDrawerOpen ] = useState(false);
-  
+  const [pantryDrawerOpen, setPantryDrawerOpen] = useState(false);
+
   const handleClose = () => {
     setAnchorEl(null);
-  }
+  };
 
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
-  //Don't display nav bar on the landing page.
-  if(location.pathname === '/')
-    return null;
-
-  return(
-    <Box sx={{ flexGrow: 1, display:'flex', justifyContent:'center', position:'fixed', width:'100%', zIndex:99 }}>
-      <AppBar 
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        display: "flex",
+        justifyContent: "center",
+        position: "fixed",
+        width: "100%",
+        zIndex: 99,
+      }}
+    >
+      <AppBar
         sx={{
-          flexDirection:'row',
-          justifyContent:'center',
-          position:'initial',
-          width:'60vw',
-          borderRadius:'0 0 10px 10px',
-          opacity: .95
+          flexDirection: "row",
+          justifyContent: "center",
+          position: "initial",
+          width: "60vw",
+          borderRadius: "0 0 10px 10px",
+          opacity: 0.95,
         }}
       >
         <Toolbar
           variant="dense"
           sx={{
-            justifyContent:'center',
-            width: 'fit-content',
+            justifyContent: "center",
+            width: "fit-content",
           }}
         >
-          <IconButton onClick={() => navigate('/')}>
+          <IconButton onClick={() => navigate("/")}>
             {/*Could use custom app icon in the future */}
-            <HomeIcon fontSize="small"/>
+            <HomeIcon fontSize="small" />
           </IconButton>
-          <LinkContainer 
-            className="nav-links"
-            sx={{width:'fit-content'}}
-          >
-            <Link 
-              component={RouterLink} 
-              to='/pantry' 
-              sx={location.pathname === '/pantry' ? {color: theme.palette.text.primary} : null}>
+          <LinkContainer className="nav-links" sx={{ width: "fit-content" }}>
+            <Link
+              component={RouterLink}
+              to="/pantry"
+              sx={
+                location.pathname === "/pantry"
+                  ? { color: theme.palette.text.primary }
+                  : null
+              }
+            >
               Pantry
             </Link>
-            <Link 
-              component={RouterLink} 
-              to='/wine' 
-              sx={location.pathname === '/wine' ? {color: theme.palette.text.primary} : null}>
+            <Link
+              component={RouterLink}
+              to="/wine"
+              sx={
+                location.pathname === "/wine"
+                  ? { color: theme.palette.text.primary }
+                  : null
+              }
+            >
               Wine
             </Link>
             <Link
               component={RouterLink}
-              to='/searchResults'
-              sx={location.pathname === '/searchResults' ? {color: theme.palette.text.primary} : null}
-              >
-                Search
-              </Link>
-              <Link
+              to="/searchResults"
+              sx={
+                location.pathname === "/searchResults"
+                  ? { color: theme.palette.text.primary }
+                  : null
+              }
+            >
+              Search
+            </Link>
+            <Link
               component={RouterLink}
-              to='/create-recipe'
-              sx={location.pathname === '/create-recipe' ? {color: theme.palette.text.primary} : null}
-              >
-                Create a recipe
-              </Link>
+              to="/create-recipe"
+              sx={
+                location.pathname === "/create-recipe"
+                  ? { color: theme.palette.text.primary }
+                  : null
+              }
+            >
+              Create a recipe
+            </Link>
           </LinkContainer>
-          
-          <IconButton 
-            onClick={handleMenu}
-          >
-            <AccountCircleIcon fontSize="small"/>
+
+          <IconButton onClick={handleMenu}>
+            <AccountCircleIcon fontSize="small" />
           </IconButton>
-          
-          <AccountMenu handleClose={handleClose} anchorEl={anchorEl}/>
+
+          <AccountMenu handleClose={handleClose} anchorEl={anchorEl} />
         </Toolbar>
       </AppBar>
-
     </Box>
-  )
-}
+  );
+};
 
 export default Navbar;
