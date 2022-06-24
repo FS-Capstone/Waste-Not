f4e0cea84b88219dfd7d2e521d1272281a9ce635
Janae Edwards <edwards.janae@gmail.com>
2022-06-24 18:05:39 -0400

    changed background photo font and transition type

--- src/components/Wine/Wine.js
+++ src/components/Wine/Wine.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState, useRef} from 'react';
-import { Box, Typography, Slide } from '@mui/material';
+import { Box, Typography, Slide, Fade } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const Wine = () => {
@@ -16,21 +16,25 @@ const Wine = () => {
     <Box 
       className='top-level-page'
       sx={{
-        width:'100vh%',
+        width:'100%',
         minHeight:'100vh',
-        backgroundImage:'url("/images/wine5.png")',
+        backgroundImage:'url("/images/pexels-grapes5.jpg")',
         backgroundSize: 'cover',
         backgroundAttachment:'fixed',
+        backgroundRepeat: 'no-repeat'
       }}
       >
-        <Box sx={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'flex-end'}}>
-          <Box ref={slideRef} sx={{display:'flex', justifyContent:'flex-end', width:'50vw', height:'30vh'}}>
-            <Slide direction='right' in={checked} container={slideRef.current}>
-              <Typography variant='h1' sx={{fontFamily: 'Alex Brush, cursive'}}>Drinking good wine with good food in good company is one of life's most civilized pleasures.</Typography>
-            </Slide>
+        <Box sx={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'flex-start', p: 3}}>
+          <Box ref={slideRef} sx={{display:'flex', justifyContent:'flex-start', width:'50vw', height:'30vh'}}>
+            {/* <Slide direction='right' in={checked} container={slideRef.current}>
+              <Typography variant='h3' sx={{fontFamily: 'Quintessential, cursive'}}>"Drinking good wine with good food in good company is one of life's most civilized pleasures."</Typography>
+            </Slide> */}
+            <Fade in={checked} {...(checked ? { timeout: 1000 } : {})}> 
+              <Typography variant='h3' sx={{fontFamily: 'Quintessential, cursive'}}>"Drinking good wine with good food in good company is one of life's most civilized pleasures."</Typography> 
+            </Fade>
           </Box>
           <Box sx={{width:'50vw', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', marginTop:'5rem'}}>
-            <Typography variant='h2' sx={{fontFamily: 'Alex Brush, cursive'}}>Wine Pairings & Recommendations </Typography>
+            <Typography variant='h2' sx={{fontFamily: 'Quintessential, cursive'}}>Wine Pairings & Recommendations </Typography>
             <Link to='/wine/recommend-wine'><button> I have a dish, recommend a wine. </button></Link>
             <Link to='/wine/recommend-dish'><button> I have the wine, recommend a dish. </button></Link>
             <Link to='/wine/recommend-brand'><button> Recommend a brand of wine. </button></Link>
