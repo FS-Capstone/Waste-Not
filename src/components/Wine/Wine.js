1884ef747ce819d84e081251c290cbc1dd2f9b22
Janae Edwards <edwards.janae@gmail.com>
2022-06-24 18:39:11 -0400

    changed transition so that links and heading transitions in instead of quote and fixed alignment

--- src/components/Wine/Wine.js
+++ src/components/Wine/Wine.js
@@ -24,21 +24,23 @@ const Wine = () => {
         backgroundRepeat: 'no-repeat'
       }}
       >
-        <Box sx={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'flex-start', p: 3}}>
-          <Box ref={slideRef} sx={{display:'flex', justifyContent:'flex-start', width:'50vw', height:'30vh'}}>
+        <Box sx={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', p: 3}}>
+          <Box ref={slideRef} sx={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', textAlign: 'center', width:'50vw', height:'20vh' }}>
             {/* <Slide direction='right' in={checked} container={slideRef.current}>
               <Typography variant='h3' sx={{fontFamily: 'Quintessential, cursive'}}>"Drinking good wine with good food in good company is one of life's most civilized pleasures."</Typography>
             </Slide> */}
-            <Fade in={checked} {...(checked ? { timeout: 1000 } : {})}> 
-              <Typography variant='h3' sx={{fontFamily: 'Quintessential, cursive'}}>"Drinking good wine with good food in good company is one of life's most civilized pleasures."</Typography> 
-            </Fade>
+            {/* <Fade in={checked} {...(checked ? { timeout: 1000 } : {})}>  */}
+              <Typography variant='h4' sx={{fontFamily: 'Quintessential, cursive', mb: '15px'}}> "Drinking good wine with good food in good company is one of life's most civilized pleasures."</Typography> 
+            {/* </Fade> */}
           </Box>
-          <Box sx={{width:'50vw', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', marginTop:'5rem'}}>
-            <Typography variant='h2' sx={{fontFamily: 'Quintessential, cursive'}}>Wine Pairings & Recommendations </Typography>
+          <Fade in={checked} {...(checked ? { timeout: 2000 } : {})}>
+          <Box sx={{width:'50vw', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', marginTop:'0.2rem'}}>
+            <Typography variant='h3' sx={{fontFamily: 'Quintessential, cursive'}}>Wine Pairings & Recommendations </Typography>
             <Link to='/wine/recommend-wine'><button> I have a dish, recommend a wine. </button></Link>
             <Link to='/wine/recommend-dish'><button> I have the wine, recommend a dish. </button></Link>
             <Link to='/wine/recommend-brand'><button> Recommend a brand of wine. </button></Link>
           </Box>
+          </Fade>
         </Box>
  
     </Box>
